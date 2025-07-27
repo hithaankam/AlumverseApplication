b2117a23ac62678313fbe7359b75dc906070a4e1
Navya Sree <balunavyasree25@gmail.com>
2025-07-27 21:53:38 +0530

    Changed layout to include feed screen in navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,7 @@ export default function RootLayout() {
         <Stack.Screen name="index" />
         <Stack.Screen name="auth" />
         <Stack.Screen name="home" />
+        <Stack.Screen name="feed" /> 
       </Stack>
     </View>
   );
