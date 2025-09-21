9c92c95599e1d9c8d9fed7c6b2a9cc119f8cdbd9
hithaankam <23wh1a1289@bvrithyderabad.edu.in>
2025-09-21 21:35:30 +0530

    updated backend domain name

--- services/AlumService.js
+++ services/AlumService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // IMPORTANT: Replace with your computer's actual local network IP address
-const API_BASE_URL = "https://alumversebackend-production.up.railway.app/api"; 
+const API_BASE_URL = "https://alumversebackend-production-8cdc.up.railway.app/api/auth"; 
 
 // --- Create a dedicated Axios instance ---
 const apiClient = axios.create({
